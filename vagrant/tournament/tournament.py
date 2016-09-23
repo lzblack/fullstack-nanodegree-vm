ab0626a74d32ae0d48afc820eff8d7a85cfb430f
Zhi Li <zli@babson.edu>
2016-09-22 22:15:05 -0400

    update tournament, using views

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -6,38 +6,39 @@
 import psycopg2
 
 
-def connect():
+def connect(database_name="tournament"):
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Connection fails.")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn = connect()
-    c = conn.cursor()
+    conn, c = connect()
     c.execute("DELETE FROM matches;")
-    c.execute("update standings set wins =0, matches=0")
     conn.commit()
     conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = connect()
-    c = conn.cursor()
+    conn, c = connect()
     c.execute("DELETE FROM players;")
     c.execute("DELETE FROM matches;")
-    c.execute("DELETE FROM standings;")
     conn.commit()
     conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = connect()
-    c = conn.cursor()
-    c.execute("SELECT count(player_id) FROM players;")
-    number = c.fetchall()[0][0]
+    conn, c = connect()
+    query = "SELECT count(player_id) FROM players;"
+    c.execute(query)
+    number = c.fetchone()[0]
     conn.close()
     return number
 
@@ -51,13 +52,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute("insert into players (name) values (%s);", (name,))
-    c.execute("select max(player_id) from players;")
-    id = c.fetchall()[0][0]
-    c.execute(
-        "insert into standings (player_id, wins, matches) values (%s, 0, 0);", (id,))
+
+    conn, c = connect()
+    query = "INSERT INTO players (name) VALUES (%s);"
+    parameter = (name,)
+    c.execute(query, parameter)
     conn.commit()
     conn.close()
 
@@ -65,8 +64,8 @@ def registerPlayer(name):
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -75,9 +74,11 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    conn = connect()
-    c = conn.cursor()
-    query = "select players.player_id, players.name, standings.wins, standings.matches from players left join standings on players.player_id = standings.player_id order by standings.wins DESC"
+    conn, c = connect()
+    query = "select players.player_id, players.name, standings.wins, standings.matches \
+    from players left join standings \
+    on players.player_id = standings.player_id \
+    order by standings.wins DESC"
     c.execute(query)
     standings = [(row[0], row[1], row[2], row[3])
                  for row in c.fetchall()]
@@ -92,16 +93,9 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute("insert into matches (winner, loser) values (%s, %s);",
-              (winner, loser,))
-    c.execute(
-        "update standings set wins=wins+1 , matches =matches +1 where player_id = (%s);", (
-            winner,))
-    c.execute(
-        "update standings set matches =matches +1  where player_id = (%s);", (
-            loser,))
+    conn, c = connect()
+    query = "insert into matches (winner, loser) values (%s, %s);"
+    c.execute(query, (winner, loser,))
     conn.commit()
     conn.close()
 
@@ -122,31 +116,15 @@ def swissPairings():
         name2: the second player's name
     """
     standings = playerStandings()
-    #[id1, id2, id3, id4, id5, id6, id7, id8] = [row[0] for row in standings]
-    conn = connect()
-    c = conn.cursor()
-    c.execute("select players.player_id, players.name from players left join standings on players.player_id = standings.player_id order by standings.wins DESC")
+    conn, c = connect()
+    query = "select players.player_id, players.name from players \
+    left join standings on players.player_id = standings.player_id \
+    order by standings.wins DESC"
+    c.execute(query)
     player_list = [(row[0], row[1])
                    for row in c.fetchall()]
     conn.close()
     match_list = [(player_list[i][0], player_list[i][1], player_list[
-                   i + 1][0], player_list[i + 1][1]) for i in range(0, len(player_list), 2)]
+                   i + 1][0], player_list[i + 1][1])
+                  for i in range(0, len(player_list), 2)]
     return match_list
-
-
-'''
-to-do list:
-
-
-    Prevent rematches between players.
-
-    Don’t assume an even number of players. If there is an odd number of players, assign one player a “bye” (skipped round). A bye counts as a free win. A player should not receive more than one bye in a tournament.
-
-    Support games where a draw (tied game) is possible. This will require changing the arguments to reportMatch.
-
-    When two players have the same number of wins, rank them according to OMW (Opponent Match Wins), the total number of wins by players they have played against.
-
-    Support more than one tournament in the database, so matches do not have to be deleted between tournaments. This will require distinguishing between “a registered player” and “a player who has entered in tournament #123”, so it will require changes to the database schema.
-    
-    You may refer to outside resources to devise your pairing algorithm. Wizards of the Coast has prepared simple instructions, and more details can be found in resources linked to in the reference section of Wikipedia's article on Swiss tournaments.
-'''
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -3,14 +3,42 @@
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
 --
+
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament
+
 CREATE TABLE players ( name TEXT, player_id SERIAL PRIMARY KEY );
 
-CREATE TABLE matches ( winner integer, 
-loser integer , match_id SERIAL PRIMARY KEY);
+CREATE TABLE matches ( winner integer REFERENCES  players(player_id), 
+loser integer REFERENCES  players(player_id), match_id SERIAL PRIMARY KEY);
 
-CREATE TABLE standings ( player_id integer, wins integer,
-matches integer);
+CREATE VIEW wins as
+ SELECT player_id, CASE WHEN number_wins IS NULL THEN 0 ELSE number_wins END
+ FROM players LEFT JOIN
+ ( SELECT winner, count(match_id) as number_wins
+ FROM matches
+ GROUP by winner) AS win
+ on players.player_id = win.winner;
 
+CREATE VIEW loses as
+ SELECT player_id, CASE WHEN number_loses IS NULL THEN 0 ELSE number_loses END
+ FROM players LEFT JOIN
+ ( SELECT loser , count(match_id) as number_loses
+ FROM matches
+ GROUP by loser) AS lose
+  on players.player_id = lose.loser;
+
+CREATE VIEW standings as
+(SELECT players.player_id, number_wins as wins, number_wins+number_loses as matches
+FROM players LEFT JOIN wins
+ON players.player_id = wins.player_id
+LEFT JOIN loses
+ON players.player_id = loses.player_id)
+
+
+
+--CASE WHEN number_wins+number_loses IS NULL THEN 0 ELSE number_wins+number_loses  END as matches
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
